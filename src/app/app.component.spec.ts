0f384b0c4827267fc1626bd40e8f009c65fb0acb
rmdcoelho <rmdcoelho@gmail.com>
2023-12-30 22:51:52 -0300

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -14,16 +14,70 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'manage-state' title`, () => {
+  it('should set the count to 0 at start', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('manage-state');
+    expect(app.count()).toEqual(0);
   });
 
-  it('should render title', () => {
+  it('should increment the count', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.increment();
+    expect(app.count()).toEqual(1);
+  });
+
+  it('should decrement the count', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.increment();
+    app.increment();
+    app.decrement();
+    expect(app.count()).toEqual(1);
+  });
+
+  it('should not decrement the count below 0', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.decrement();
+    expect(app.count()).toEqual(0);
+  });
+
+  it('should reset the count', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.increment();
+    app.reset();
+    expect(app.count()).toEqual(0);
+  });
+
+  it('should render the count', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, manage-state');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.count').textContent).toContain('0');
+  });  
+
+  it('should render the double of the count', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.double').textContent).toContain('0');
   });
+
+  it('should render the binary power of the count', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.binary-power').textContent).toContain('1');
+  });
+
+  it('should notify the count', fakeAsync(() => {
+    spyOn(console, 'log'); // The spy is a function that replaces the original function and should be created before the component.
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.increment();
+    tick(1000);
+    expect(console.log).toHaveBeenCalled();
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatChipsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'manage-state';
+
+  count = signal(0);
+  
+  constructor() {
+    this.notify();
+  }
+
+  public increment = () => this.count.update(c => c + 1);
+
+  public decrement = () => this.isCountGreaterThanZero() ? this.count.update(c => c - 1) : null;
+
+  public reset = () => this.count.set(0);
+  
+  public notify() {
+    console.log("Notify effect triggered with count: ", this.count());
+     effect(() => console.log(this.count()));
+  }
+  
+  get double() {
+    return computed(() => this.count() * 2);
+  }
+
+  get binaryPower() {
+    return computed(() => 2 ** this.count());
+  }
+
+  private isCountGreaterThanZero = () => this.count() > 0;
 }
